c38c29974f905c7e030c6aedde889a43c87928f6
Henrique Carvalho de Souza <niguiealmeida@hotmail.com>
2021-10-01 12:31:57 -0300

    webpack & wbpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,52 @@
+const modoDev = process.env.NODE_ENV !== 'production'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { Module } = require('webpack')
 const webpack = require('webpack')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    devServer:{
+        contentBase: "./public",
+        port: 9000
+    },
+
+    optimization: {
+        minimizer: [            
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+
     module:{
         rules: [{
-            test: /\.css$/, 
+            test: /\.s?[ac]ss$/, 
             use: [
-                'style-loader', //Adiciona CSS na DOM injetando a tag <style>
-                'css-loader' //Interpreta @import, url()...
+                MiniCssExtractPlugin.loader,
+                //'style-loader', //Adiciona CSS na DOM injetando a tag <style>
+                'css-loader', //Interpreta @import, url()...
+                'sass-loader',
             ]
+        },{
+            test: /\.(png|svg|jpg|gif)$/, 
+            use: ['file-loader']
         }]
     }
 }
